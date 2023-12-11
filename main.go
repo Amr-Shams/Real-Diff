421f55219c0274f284998ee551136bf089ef2f04
Amr Shams <69462228+Amr-Shams@users.noreply.github.com>
2023-12-11 21:57:50 +0200

    [+Update]:Done with the extraction and removing the commments

--- main.go
+++ main.go
@@ -7,73 +7,123 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/stoicperlman/fls"
 )
 
-func getFunctionNames(cFilePath string) ([]string, error) {
-	// Run ctags command to generate tags for the C file
+type Function struct {
+	Name string
+	Body string
+	Line int
+}
+
+func main() {
+	cFilePath := "../foo.c"
+	start := time.Now()
+	err := cleanFile(cFilePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Time elapsed: ", elapsed)
+}
+
+func cleanFile(filePath string) error {
+	f, err := fls.OpenFile(filePath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Seek(0, io.SeekStart)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	scanner := bufio.NewScanner(f)
+	functions, err := getFunctions(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+
+	lineNumbers := extractLineNumbers(functions)
+	sort.Slice(functions, func(i, j int) bool {
+		return functions[i].Line < functions[j].Line
+	})
+
+	processFunctions(scanner, functions, lineNumbers)
+	fmt.Println(functions)
+	return nil
+}
+
+func getFunctions(cFilePath string) ([]Function, error) {
 	cmd := exec.Command("ctags", "-x", "--c-kinds=f", cFilePath)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, fmt.Errorf("error running ctags: %v", err)
 	}
 
-	// Parse the output to extract function names
-	scanner := bufio.NewScanner(strings.NewReader(string(output)))
-	functionNames := make([]string, 0)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		// fmt.Println(line)
-		// split the words in the line into a list of words
-		words := strings.Fields(line)
-		fmt.Println(words[0], words[2])
-		// append as well the third word which is the function line number
-		functionNames = append(functionNames, words[0]+" "+words[2])
-
+	functions := []Function{}
+	for _, line := range strings.Split(string(output), "\n") {
+		lineList := strings.Fields(line)
+		if len(lineList) > 0 {
+			lineNumber, err := strconv.Atoi(lineList[2])
+			if err != nil {
+				return nil, fmt.Errorf("error converting line number to integer: %v", err)
+			}
+			functionSignature := extractFunctionSignature(lineList)
+			functions = append(functions, Function{Name: functionSignature, Line: lineNumber})
+		}
 	}
 
-	return functionNames, nil
-
-}
-func removeComments(input string) string {
-	// Regular expression to match C-style comments
-	commentRegex := regexp.MustCompile(`/\*[\s\S]*?\*/|//.*?$`)
-	return commentRegex.ReplaceAllString(input, "")
+	return functions, nil
 }
 
-func readLineFromFile(filePath string, lineNumber int) (string, error) {
-	f, err := fls.OpenFile(filePath, os.O_RDONLY, 0)
-	if err != nil {
-		return "", err
+func extractFunctionSignature(lineList []string) string {
+	functionSignature := strings.Join(lineList[4:], " ")
+	re := regexp.MustCompile(`^[\w\s]*\(\)`)
+	matches := re.FindStringSubmatch(functionSignature)
+	if len(matches) > 0 {
+		functionSignature = matches[0]
 	}
-	defer f.Close()
+	return functionSignature
+}
 
-	// Seek to the beginning of the specified line
-	_, err = f.Seek(0, io.SeekStart)
-	if err != nil && err != io.EOF {
-		return "", err
+func extractLineNumbers(functions []Function) []int {
+	lineNumbers := []int{}
+	for _, function := range functions {
+		lineNumbers = append(lineNumbers, function.Line)
 	}
+	return lineNumbers
+}
 
-	scanner := bufio.NewScanner(f)
-	var insideFunction bool
-
-	// creat a buffer to store the function body
-	functionBody := strings.Builder{}
+func processFunctions(scanner *bufio.Scanner, functions []Function, lineNumbers []int) {
+	var insideFunction, startFunction bool
+	var functionBody strings.Builder
 	depth := 0
+	index := 0
 
 	for linNum := 1; scanner.Scan(); linNum++ {
 		line := scanner.Text()
+		if !startFunction && isStartOfFunction(linNum, lineNumbers) {
+			startFunction = true
+		}
+		if !startFunction {
+			continue
+		}
+
 		for _, char := range line {
 			if char == '{' {
 				depth++
-				if !insideFunction && linNum == lineNumber {
+				if !insideFunction {
 					insideFunction = true
 				}
-
 			}
 			if insideFunction {
 				functionBody.WriteRune(char)
@@ -82,35 +132,43 @@ func readLineFromFile(filePath string, lineNumber int) (string, error) {
 				depth--
 				if depth == 0 && insideFunction {
 					insideFunction = false
+					startFunction = false
+					functionBodyString := cleanFunctionBody(functionBody.String())
+					functions[index].Body = functionBodyString
+					index++
+					functionBody.Reset()
 					break
 				}
 			}
 		}
+		functionBody.WriteRune('\n')
 	}
-	// remove comments from the function body
-	// function body before removing comments
-	fmt.Println(functionBody.String())
-
-	return removeComments(functionBody.String()), nil
 }
 
-func main() {
-	cFilePath := "../foo.c"
-
-	functionNames, err := getFunctionNames(cFilePath)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	for _, LineNumber := range functionNames {
-		// convert the LineNumber to an int
-		linNum, err := strconv.Atoi(strings.Split(LineNumber, " ")[1])
-		println(linNum)
-		functionBody, err := readLineFromFile(cFilePath, linNum)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
+func isStartOfFunction(linNum int, lineNumbers []int) bool {
+	for _, lineNumber := range lineNumbers {
+		if linNum == lineNumber {
+			return true
 		}
-		fmt.Println(functionBody)
 	}
+	return false
+}
+
+func cleanFunctionBody(functionBody string) string {
+	functionBody = string(removeCStyleComments([]byte(functionBody)))
+	functionBody = string(removeCppStyleComments([]byte(functionBody)))
+	functionBody = strings.ReplaceAll(functionBody, "\n", "")
+	functionBody = strings.ReplaceAll(functionBody, "\t", "")
+	functionBody = strings.ReplaceAll(functionBody, " ", "")
+	return functionBody
+}
+
+func removeCStyleComments(content []byte) []byte {
+	ccmt := regexp.MustCompile(`/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/`)
+	return ccmt.ReplaceAll(content, []byte(""))
+}
+
+func removeCppStyleComments(content []byte) []byte {
+	ccmt := regexp.MustCompile(`//.*$`)
+	return ccmt.ReplaceAll(content, []byte(""))
 }
