ce89f454da949d9a11a18a048aae3ed57cc33d23
Amr Shams <69462228+Amr-Shams@users.noreply.github.com>
2023-12-20 14:06:17 +0200

    [+Udate]:

--- client.c
+++ client.c
@@ -1,10 +1,29 @@
-int main()
-int main()
-int main()
-int main()
-int main()
-int main()
-void sub()
-int bar()
-int tt()
-void func()
+#include <stdio.h>
+
+#define printValue(x) _Generic((x), \
+    int: printInt, \
+    double: printDouble, \
+    char*: printString \
+)(x)
+
+void printInt(int x
+,int y) {
+    printf("Integer: %d\n", x);
+}
+
+void printDouble(double x) {
+    printf("Double: %lf\n", x);
+}
+
+void printString(char* x) {
+    printf("String: %s\n", x);
+}
+
+int main() {
+    printValue(42);
+    printValue(3.14);
+    printValue("Hello");
+
+    return 0;
+}
+
